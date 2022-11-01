c62c415027268ef48fe026eb475b059809ae41a9
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-11-01 23:22:13 +0100

    Ajout app simple Express et lien avec server NodeJS

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.use((request, response) => {
+  response.json({ message: "Votre requête a bien été reçue !" });
+});
+
+module.exports = app;
