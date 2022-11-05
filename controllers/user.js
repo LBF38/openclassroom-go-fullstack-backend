e9dfb84916a5bb3f98a5e9a76e4e41fbd24cae7d
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-11-05 14:15:26 +0100

    Added login controller

--- controllers/user.js
+++ controllers/user.js
@@ -18,4 +18,39 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user === null) {
+        res
+          .status(401)
+          .json({ message: "Paire identifiant/mot de passe incorrecte" });
+      } else {
+        bcrypt.compare(req.body.password, user.password).then((valid) => {
+          if (!valid) {
+            res
+              .status(401)
+              .json({ message: "Paire identifiant/mot de passe incorrecte" });
+          } else {
+            res.status(200).json({
+              userId: user._id,
+              token: "TOKEN",
+            });
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      bcrypt
+        .compare(hash)
+        .then(res.status(200).json({ message: "Authenticated!" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
