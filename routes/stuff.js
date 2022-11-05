7c917d708d8182e5f899e8a56cfac3c16ebd75d9
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-11-05 15:31:38 +0100

    Implemented the POST and PUT methods
    They can manage user images uploaded via frontend.

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const router = express.Router();
+const multer = require("../middleware/multer-config");
 
 const stuffController = require("../controllers/stuff");
 
-router.post("/", auth, stuffController.createThing);
+router.post("/", auth, multer, stuffController.createThing);
 router.get("/", auth, stuffController.getAllThings);
 router.get("/:id", auth, stuffController.getOneThing);
-router.put("/:id", auth, stuffController.modifyThing);
+router.put("/:id", auth, multer, stuffController.modifyThing);
 router.delete("/:id", auth, stuffController.deleteThing);
 
 module.exports = router;
